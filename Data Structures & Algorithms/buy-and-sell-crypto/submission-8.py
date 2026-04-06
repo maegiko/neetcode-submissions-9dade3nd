b95a0f2077307cda7d19970195870d1bcbe5853e
Kenneth Koon <kennethk.dev@gmail.com>
2026-04-06 21:00:21 +1000

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0
+        right = 1
+        maxProfit = 0
+        
+        while right < len(prices):
+            if prices[right] > prices[left]:
+                profit = prices[right] - prices[left]
+                maxProfit = max(maxProfit, profit)
+            elif prices [right] < prices[left]:
+                left = right
+            
+            right += 1
+        
+        return maxProfit
