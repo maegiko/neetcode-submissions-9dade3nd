d19a2e1031ac16d36a3faacaae23ca5b4270b83a
Kenneth Koon <kennethk.dev@gmail.com>
2026-09-05 13:03:21 +1000

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        seen = defaultdict(set)
+        
+        for i in range(0, 9):
+            for j in range(0, 9):
+                key = f"{i} row"
+                val = board[i][j]
+
+                if val in seen[key]:
+                    return False
+                
+                if val != '.':
+                    seen[key].add(val)
+
+                small_i = i // 3
+                small_j = j // 3
+
+                key = f"{small_i}, {small_j}"
+
+                if val in seen[key]:
+                    return False
+                
+                if val != '.':
+                    seen[key].add(val)
+        
+        for i in range(0, 9):
+            for j in range(0, 9):
+                key = f"{i} col"
+                val = board[j][i]
+
+                if val in seen[key]:
+                    return False
+                
+                if val != '.':
+                    seen[key].add(val)
+        
+        return True
